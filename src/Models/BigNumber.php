d145c21655e2e465c9b6280897ab2212d5319411
João Paulo dos Santos Ferreira <joao.paulo@fmd.ag>
2026-03-02 11:13:48 -0300

    feat(models): adiciona escopos padrão e ordenação em BigNumber
    Adiciona os escopos `isActive` e `sort` ao modelo BigNumber para facilitar
    o uso de condições e ordenação padrão em consultas. Inclui também
    atributos com ordem e critérios definidos para sort automáticos.

--- src/Models/BigNumber.php
+++ src/Models/BigNumber.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Agenciafmd\BigNumbers\Models;
 
 use Agenciafmd\BigNumbers\Database\Factories\BigNumberFactory;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -19,10 +20,38 @@ final class BigNumber extends Model implements AuditableContract
 {
     use Auditable, HasFactory, Prunable, SoftDeletes;
 
+    protected array $defaultSort = [
+        'sort' => 'asc',
+        'big_number' => 'asc',
+    ];
+
     public function prunable(): Builder
     {
         return self::query()
             ->where('deleted_at', '<=', now()->subDays(30));
+    }
+
+    #[Scope]
+    protected function isActive(Builder $query): void
+    {
+        $query->where('is_active', true);
+    }
+
+    #[Scope]
+    protected function sort(Builder $query): void
+    {
+        $defaultSort = $this->defaultSort ?? [
+            'is_active' => 'desc',
+            'name' => 'asc',
+        ];
+
+        foreach ($defaultSort as $field => $direction) {
+            if ($field === 'sort') {
+                $query->orderByRaw('-' . $query->qualifyColumn('sort') . ' DESC');
+            } else {
+                $query->orderBy($query->qualifyColumn($field), $direction);
+            }
+        }
     }
 
     protected function casts(): array
