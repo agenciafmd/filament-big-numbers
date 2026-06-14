6ad96f429a6fa366c155f057562507791177e1c0
Irineu Junior <irineu@fmd.ag>
2026-06-13 21:57:25 -0300

    refactor(src): organizar schema com Grid e Group

--- src/Resources/BigNumbers/Schemas/BigNumberForm.php
+++ src/Resources/BigNumbers/Schemas/BigNumberForm.php
@@ -7,6 +7,8 @@ namespace Agenciafmd\BigNumbers\Resources\BigNumbers\Schemas;
 use Agenciafmd\Admix\Resources\Infolists\Components\DateTimeEntry;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -16,35 +18,43 @@ final class BigNumberForm
     {
         return $schema
             ->components([
-                Section::make(__('General'))
+                Grid::make(3)
                     ->schema([
-                        TextInput::make('big_number')
-                            ->translateLabel()
-                            ->autofocus()
-                            ->required()
-                            ->maxLength(255),
-                        TextInput::make('description')
-                            ->translateLabel()
-                            ->required()
-                            ->maxLength(255),
-                        //                        TextInput::make('sort')
-                        //                            ->translateLabel()
-                        //                            ->numeric(),
+                        Group::make([
+                            Section::make(__('General'))
+                                ->schema([
+                                    TextInput::make('big_number')
+                                        ->translateLabel()
+                                        ->autofocus()
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('description')
+                                        ->translateLabel()
+                                        ->required()
+                                        ->maxLength(255),
+                                    //                        TextInput::make('sort')
+                                    //                            ->translateLabel()
+                                    //                            ->numeric(),
+                                ])
+                                ->collapsible()
+                                ->columns()
+                                ->columnSpan(2),
+                        ])
+                            ->columnSpan(2),
+                        Group::make([
+                            Section::make(__('Information'))
+                                ->schema([
+                                    Toggle::make('is_active')
+                                        ->translateLabel()
+                                        ->default(true),
+                                    DateTimeEntry::make('created_at'),
+                                    DateTimeEntry::make('updated_at'),
+                                ])
+                                ->collapsible()
+                                ->columns(),
+                        ]),
                     ])
-                    ->collapsible()
-                    ->columns()
-                    ->columnSpan(2),
-                Section::make(__('Information'))
-                    ->schema([
-                        Toggle::make('is_active')
-                            ->translateLabel()
-                            ->default(true),
-                        DateTimeEntry::make('created_at'),
-                        DateTimeEntry::make('updated_at'),
-                    ])
-                    ->collapsible()
-                    ->columns(),
-            ])
-            ->columns(3);
+                    ->columnSpanFull(),
+            ]);
     }
 }
